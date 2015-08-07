b76a590e91e1c60728f4b6d94b2ce444efd14189
Andres Diaz <andreselduende@gmail.com>
2015-08-07 09:50:23 -0500

    Add SENGRID MAIL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,9 +43,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              ENV['SMTP_SERVER'],
     port:                 587,
-    domain:               ENV['DOMAIN'],
+    domain:               ENV['SMTP_DOMAIN'],
     user_name:            ENV['SMTP_USER_NAME'],
     password:             ENV['SMTP_USER_PASSWORD'],
     authentication:       'plain',
