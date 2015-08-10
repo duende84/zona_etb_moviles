18039a8dc4d7f0c1acf344d5f2105297b60d4b27
Andres Diaz <andreselduende@gmail.com>
2015-08-10 10:58:41 -0500

    Update mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,6 @@ class UserMailer < ApplicationMailer
     @url  = qr_img
     @code = code.code
     attachments['code.png'] = open(qr_img).read()
-    mail(to: @user.email, subject: 'Bienvenid@ a la zona etb móviles')
+    mail(to: @user.email, subject: 'Bienvenid@ a la zona ETB móviles')
   end
 end
